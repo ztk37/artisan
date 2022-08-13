54985cfc5beb613d25fb35224180e759a312707e
ztk37 <ztk37.contact@gmail.com>
2022-08-13 18:14:50 +0200

    impl: update argument handling

---
+++ src/cli.rs
@@ -0,0 +1,1 @@
+// TODO: Move clap cli logic to cli.rs
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::{Parser, Subcommand};
+
+const VERSION: &str = "v0.0.0";
+
+#[derive(Parser, Debug)]
+#[clap(
+    name = "repo",
+    version = VERSION
+)]
+pub struct Arguments {
+    #[clap(subcommand)]
+    pub command: Command
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    Init,
+    Version
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+
+    match args.command {
+        Command::Init => todo!(),
+        Command::Version => println!("{}", VERSION)
+    }
 }
