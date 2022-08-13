384410e9643584c99d87ecb889e4cdf4eac96711
ztk37 <ztk37.contact@gmail.com>
2022-08-13 22:48:34 +0200

    refactor: move argument parsing to cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -1,1 +1,30 @@
-// TODO: Move clap cli logic to cli.rs
+use clap::{Parser, Subcommand};
+
+use crate::cmds;
+
+#[derive(Parser, Debug)]
+#[clap(
+    name = "repo",
+    version = "v0.0.0",
+    about = "A tool for doing chore work inside your repositories."
+)]
+pub struct Arguments {
+    #[clap(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    #[clap(about = "Init repo in the current directory")]
+    Init,
+}
+
+pub fn parse_args() -> Arguments {
+    return Arguments::parse()
+}
+
+pub fn run(args: Arguments) -> Result<(), String> {
+    match args.command {
+        Command::Init => cmds::init().map_err(|_err| String::from("")),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,16 @@
+use std::process::exit;
+
+mod cli;
 mod cmds;
 
-use clap::{Parser, Subcommand};
-
-#[derive(Parser, Debug)]
-#[clap(
-    name = "repo",
-    version = "v0.0.0",
-    about = "A tool for doing chore work inside your repositories."
-)]
-pub struct Arguments {
-    #[clap(subcommand)]
-    pub command: Command,
-}
-
-#[derive(Subcommand, Debug)]
-pub enum Command {
-    #[clap(about = "Init repo in the current directory")]
-    Init,
-}
-
 fn main() {
-    let args = Arguments::parse();
+    let args = cli::parse_args();
 
-    match args.command {
-        Command::Init => {
-            if let Err(err) = cmds::init() {
-                println!("{:?}", err)
-            }
+    match cli::run(args) {
+        Ok(()) => exit(0),
+        Err(err) => {
+            println!("{:?}", err);
+            exit(1)
         }
     }
 }
