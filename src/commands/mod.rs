2a3464da00179207fb2944536d0903200530a3e4
ztk37 <ztk37.contact@gmail.com>
2022-09-10 21:14:44 +0200

    refactor: move run_command to command module

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,1 +1,9 @@
+use crate::{cli::RootCommand, error::CliError};
+
 pub mod new;
+
+pub fn run(cmd: RootCommand) -> Result<(), CliError> {
+    match cmd {
+        RootCommand::New(options) => new::run(options),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,12 @@ use std::process::exit;
 
 use clap::Parser;
 
-use repo::{
-    cli::{Options, RootCommand},
-    commands,
-    error::CliError,
-};
+use repo::{cli::Options, commands};
 
 fn main() {
     let opts = Options::from_args();
 
-    let exit_code = match run_command(opts.command) {
+    let exit_code = match commands::run(opts.command) {
         Err(message) => {
             let code = 1;
             println!("exited with code {} - {:?}", code, message);
@@ -21,10 +17,4 @@ fn main() {
     };
 
     exit(exit_code);
-}
-
-fn run_command(cmd: RootCommand) -> Result<(), CliError> {
-    match cmd {
-        RootCommand::New(options) => commands::new::run(options),
-    }
 }
