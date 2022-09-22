12e937f3217f622f821945c8a4a538f43d01c69b
ztk37 <ztk37.contact@gmail.com>
2022-09-22 19:31:03 +0200

    update: add artisan home dir support

--- src/commands/new.rs
+++ src/commands/new.rs
@@ -1,12 +1,13 @@
-use crate::{cli::NewCommand, error::CliResult, template::Template};
-use std::path::PathBuf;
+use crate::{cli::NewCommand, error::CliResult, find_artisan_home_location, template::Template};
 
 pub fn run(options: &NewCommand) -> CliResult {
+    let home = find_artisan_home_location()?;
+
+    let template_file_content = std::fs::read(home.join("templates").join(&options.template))?;
+
     std::fs::create_dir(&options.name)?;
 
-    let file_content = std::fs::read(PathBuf::from("templates").join(&options.template))?;
-
-    let template: Template = toml::from_slice(file_content.as_ref())?;
+    let template: Template = toml::from_slice(&template_file_content.as_ref())?;
 
     template.templates.into_iter().for_each(|template| {
         if let Err(err) = template.generate(&options.name) {
