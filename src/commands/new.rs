2c91cfe25a171df875dbd979b81021629cef111e
ztk37 <ztk37.contact@gmail.com>
2022-09-22 20:02:29 +0200

    refactor: improve error message

--- src/commands/new.rs
+++ src/commands/new.rs
@@ -2,8 +2,16 @@ use crate::{cli::NewCommand, error::CliResult, find_artisan_home_location, templ
 
 pub fn run(options: &NewCommand) -> CliResult {
     let home = find_artisan_home_location()?;
+    let template_dir = home.join("templates");
+    let template_file_name = template_dir.join(&options.template);
 
-    let template_file_content = std::fs::read(home.join("templates").join(&options.template))?;
+    let template_file_content = std::fs::read(&template_file_name).map_err(|_| {
+        format!(
+            "Can't find template {:?} in {:?}",
+            options.template,
+            template_dir.as_path(),
+        )
+    })?;
 
     std::fs::create_dir(&options.name)?;
 
